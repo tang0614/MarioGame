0bdc3c740ea6e1fbfcc1a6ce924698578dfec306
tang0614 <tang0614@bu.edu>
2020-04-14 23:02:10 -0400

    upload

--- public/js/loader/loadLevel.js
+++ public/js/loader/loadLevel.js
@@ -17,17 +17,19 @@ export function loadLevel(name){
     })
     .then(([levelFile,BackGroundSprite,mario_entity])=>{
         const level = new Level();
-        //lowested layer- set up matrix with range and name
-        //give each element inside the matrix a name/value
-        const collistionGrid = createCollisionGrid(levelFile.tiles,levelFile.patterns);
-        level.setCollisionGrid(collistionGrid); //become a tile resolver inside level
-
-        
-       
+ 
+        //merge all tiles from all layers into the same layer
+        const mergedTiles = levelFile.layers.reduce((mergedTiles,layer)=>{
+            return mergedTiles.concat(layer.tiles);
+        },[]);
+    
         //layer one - drawing background on context according to element's name and posiiton in matrix
-        const backgroundGrid = createBackgroundGrid(levelFile.tiles,levelFile.patterns);
-        const background_draw_function = getBackgroundLayer(level,backgroundGrid,BackGroundSprite);
-        level.compo.layers.push(background_draw_function);
+        levelFile.layers.forEach(layer=>{
+            const backgroundGrid = createBackgroundGrid(layer.tiles,levelFile.patterns);
+            const background_draw_function = getBackgroundLayer(level,backgroundGrid,BackGroundSprite);
+            level.compo.layers.push(background_draw_function);
+
+        })
         
         //layer two - drawing entities on context
         level.entities.add(mario_entity);
@@ -35,6 +37,9 @@ export function loadLevel(name){
         level.compo.layers.push(mario_draw_function);
 
         //layer three -top layer- need entities and backgroud-sprite to draw collision
+        const collistionGrid = createCollisionGrid(mergedTiles,levelFile.patterns);
+        level.setCollisionGrid(collistionGrid); //become a tile resolver inside level
+
         const draw_collision_function =createCollisionLayer(level);
         level.compo.layers.push(draw_collision_function);
         
